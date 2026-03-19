e8eedc25ff37d87fcf7c77d4a139e822d0962de2
blsgamesy-hub <bls.game.sy@gmail.com>
2026-03-19 14:36:21 +0900

    Add files via upload

--- sw_routine.js
+++ sw_routine.js
@@ -1,4 +1,4 @@
-const CACHE = 'routine-v1';
+const CACHE = 'routine-v2';
 const FILES = [
   './routine.html',
   './routine_icon.png',
@@ -9,6 +9,18 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(FILES))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
